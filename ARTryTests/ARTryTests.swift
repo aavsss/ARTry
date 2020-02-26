becec3d45dd8b3c3ce369059618e369ddb8d5d00
Aavash Sthapit <aavashsthapit@gmail.com>
2020-02-26 17:39:16 -0600

    First

---
+++ ARTryTests/ARTryTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARTryTests.swift
+//  ARTryTests
+//
+//  Created by Aavash Sthapit on 2/25/20.
+//  Copyright © 2020 Aavash Sthapit. All rights reserved.
+//
+
+import XCTest
+@testable import ARTry
+
+class ARTryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
